ecf90e19aa92c311582e1b0aac7895b4eb106551
fabiog96 <39191508+fabiog96@users.noreply.github.com>
2025-04-18 18:38:23 +0200

    Refactor background.js for improved readability and organization

--- background.js
+++ background.js
@@ -1,29 +1,36 @@
 chrome.action.onClicked.addListener(async() => {
+    // Get the current active tab in the window
     const [tab] = await chrome.tabs.query({ 
         active: true,
         currentWindow: true 
     });
 
-    console.log("Current tab => ",tab);
-
+    
+    // Check if the tab is valid
     if(!tab.url || !tab.id ) return;
-
+    
+    console.log("Current tab => ",tab);
+    
     const url = new URL(tab.url);
     const origin = url.origin;
     
+    // Clear cookies and localStorage for the current tab's origin
     chrome.browsingData.remove({
         "origins": [origin]
     }, {
         "cookies": true,
         "localStorage": true,
     }, () => {
+        // Reload the current tab to reflect the changes
         chrome.tabs.reload(tab.id);
+        // Optionally, you can create a notification to inform the user
         createNotification(origin);
         console.log(`Browsing data cleared for origin: ${origin}`);
     });
 })
 
 
+// Function to create a notification
 function createNotification(url){
     chrome.notifications.create({
         type: 'basic',
